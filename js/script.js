e3eeff34711990b52550d6dbc9953c382e19d084
Joao Vitor Pires Leite <joao.leite@mendesholler.com.br>
2026-06-18 11:33:44 -0300

    Feature: Sistema de abas, lista real de 313 participantes, ganhadores com data e overlay para print

--- js/script.js
+++ js/script.js
@@ -1,18 +1,334 @@
 let defaultParticipantes = [
-    "João Silva",
-    "Maria Oliveira",
-    "Carlos Eduardo",
-    "Ana Beatriz",
-    "Roberto Carlos",
-    "Fernanda Lima",
-    "Lucas Mendes",
-    "Juliana Paes",
-    "Marcos Holler",
-    "Camila Pitanga"
+    "JOSE RICARDO LOSSIO REZENDE",
+    "SERGIO RICARDO LOPES COLHADO",
+    "LITZA CRISTINA L. DE OLIVEIRA F. BATISTA",
+    "JAIR PEREIARA DE ASSIS FILHO",
+    "JORGE LUIS PEREIRA HENRIQUE ALVES",
+    "MYRIAM REZENDE TEIXEIRA COELHO CAPARROS",
+    "ELAINE CRISTINA PARIZOTTO",
+    "THAIS DOMINGUES MENDES",
+    "RICARDO ROCHA PATEZ",
+    "JEFERSON DUARTE GONCALVES",
+    "KAINON UMBELINO DOS SANTOS",
+    "RAFAEL EDUARDO DE OLIVEIRA",
+    "ODAIR SILVA DOS SANTOS",
+    "DENIS SANTOS DE SANTANA",
+    "SANDERLEI SOUSA DOS SANTOS",
+    "LUIS FELIPE DA CONCEICAO",
+    "DIOGO DE MELLO SILVA",
+    "RENE DOS SANTOS PEREIRA FILHO",
+    "JONATAS SOARES RODRIGUES",
+    "THALLYSON LIMA DA SILVA",
+    "LAFAIETE PEREIRA DE SOUZA",
+    "ANDERSON QUIRINO BRASILEIRO",
+    "EMANUEL GUSTAVO BARCAROLO",
+    "ALAN AUGUSTO HUBNER RIBEIRO",
+    "ALDEAN ALENCAR DE SOUSA",
+    "ALDRLAN ALENCAR DE SOUSA",
+    "DIEGO BRIZOLA DE OLIVEIRA",
+    "ERICK SANTOS DA SILVA",
+    "EZEQUIEL LEITE DOS SANTOS",
+    "FELIPE SCARPONI MUNIZ",
+    "JOAQUIM DE ALMEIDA",
+    "SANTIAGO SOUSA DOS SANTOS",
+    "DAVID ARAUJO FLORES",
+    "MAURICIO CORREIA JUNIOR",
+    "FABIO DE ANDRADE SILVA",
+    "ELEN APARECIDA DE MOURA SOUZA",
+    "CAIO VINIARCZYK PINHEIRO",
+    "ISABELLA ABDALA DE FREITAS",
+    "FABIO DA SILVA PACHECO",
+    "MARCIA AMORIM DE SANTANA",
+    "EDSON DE ARAUJO TEIXEIRA",
+    "RAFAEL COMPORTO MONTERO",
+    "MARCOS ALESSANDRO ROCHA SANTOS",
+    "CLAUDINEI MARQUES",
+    "ADILSON MARIO BELCASTRO",
+    "CAROLINA GEROLDO TORRES",
+    "ROBERTA OLIVEIRA RAPOSO LIMA",
+    "ROGERIO MARTINS",
+    "MARCEL HENRIQUE SIEDLER POMPIANI",
+    "TAYSON JOSE LOPES FARIAS",
+    "ANTONIO AGRIMAR CLIMERIO SILVA",
+    "ERNESTO JOSE DE JESUS SILVA",
+    "GABRIEL RIBEIRO MONTINO",
+    "CARLOS EVANDRO ALVES DOS SANTOS",
+    "JOAO CARLOS ALVES ARAUJO LOURENCO GARCIA",
+    "JOSE DOS SANTOS FILHO",
+    "CAMILO DE LELES RODRIGUES CORREA",
+    "LUIZ FERNANDES DANTAS DE SOUSA",
+    "SEBASTIAO ALVES DE QUEIROZ",
+    "GUSTAVO HENRIQUE VEIGA LARA MENDES",
+    "LUIZ CARLOS DA ROCHA UENO",
+    "JOAO BATISTA DA SILVA MUNIZ",
+    "SERGIO CALLEGARIS",
+    "EDSON FERNANDES PEREIRA",
+    "JOSE DONIZETE LEITE DE LIMA",
+    "ALVARO DOMINGOS LOPES",
+    "ANTONIO LUIZ DA SILVA SOUZA",
+    "MARCELO PEREIRA DA SILVA",
+    "ORLANDO LOPES LIMA",
+    "PAULA FERNANDA CINTRA",
+    "MARCO AURELIO DE OLIVEIRA",
+    "ROMARIO SOARES MARQUES",
+    "ALESSANDRO DE SOUZA MAZZARI",
+    "JULIANE STECKER NASCIMENTO",
+    "ANDERSON PAULINO NAGUARA",
+    "ERASMO CARLOS ALVES DE JESUS",
+    "GABRIEL WENDEL DE ALMEIDA",
+    "FRANCISCO ANTONIO DA SILVA VIEIRA",
+    "MARIA FERNANDA DE SOUZA",
+    "ALEXANDRE PLACIDO DA SILVA OLIVEIRA",
+    "HELBERT LUIZ COSTA",
+    "JOSE AUGUSTO SOUSA GOES",
+    "EDVALDO CASTRO NUNES",
+    "EDEZIO MARQUES CERQUEIRA",
+    "EDILSON MARQUES DA ROCHA",
+    "MARTIN HERMENEGILDO CAMACHO GAMBOA",
+    "VALDEMBERG BEZERRA PEREIRA",
+    "CLODOALDO JOSE DE OLIVEIRA",
+    "MATHEUS SANTANA CONTINI",
+    "ALINE CARVALHO RIBEIRO",
+    "FRANCIVALDO DOS SANTOS MIRANDA",
+    "HERCULES OLIVEIRA FAUSTINO",
+    "DILSON DE CARVALHO",
+    "JOAO BATISTA FERREIRA MACHADO",
+    "TALYANDERSON GONCALVES CORREA",
+    "DENIS ROCCO DOMINGOS",
+    "JAMERSON GONCALVES SIQUEIRA",
+    "ALINE SOUZA SANTANA",
+    "VICTOR HADDAD BIGATON",
+    "LEONARDO GRECCO BARBOSA",
+    "OZEAS ALVES DE SOUZA",
+    "PASCOAL CARLOS DE JESUS FILHO",
+    "ANTONIO FRANCISCO CORREA JUNIOR",
+    "THOMAZ DE AQUINO",
+    "JOSE VANDSON DE LIMA MARTINS",
+    "MARCIO DA SILVA FERRAZ",
+    "FELIPE PASSOS DA SILVA",
+    "CRISTIANO CONCEICAO DA SILVA",
+    "RONEI SILVA GOUVEIA",
+    "JOSE LUIZ SIQUEIRA DE FRANCA",
+    "KAIQUE SANTIAGO DUARTE",
+    "DANIEL DA MATA ALVES",
+    "GABRIEL DA SILVA OLIVEIRA",
+    "GABRIELA MATOS DA SILVA",
+    "IGOR TAVARES DA SILVA",
+    "ANDERSON PEREIRA DE BARROS",
+    "MARIA EDUARDA OLIVEIRA GOMES",
+    "CLAUDIA CORREA DOGO",
+    "GILIARDE XISTO SILVA",
+    "LUIZ FELIPE BLUVOL WALKER SANTOS",
+    "DIEGO SALGO E SILVA",
+    "RODRIGO DANTAS DOS SANTOS",
+    "BRUNO CESAR AURIEME",
+    "DAMARIS ALEXANDRE SANCHES",
+    "LEANDRO FERREIRA CAVALCANTI",
+    "ANTHLER GEORGE PEREIRA SOBRINHO",
+    "GUILHERME ALCANTARA ALI KHALIL",
+    "JOSE DE RIBAMAR RODRIGUES GAMA",
+    "THIAGO GREGORIO GOMES ERMENEGILDO",
+    "GILDASIO DOS SANTOS",
+    "GABRIEL GOMES DE FRANCA",
+    "CARLOS ROBERTO DOS SANTOS",
+    "EZEQUIEL DOS REIS VITORINO",
+    "ANTONIO VINICIUS LIMA DE SENA COSTA",
+    "APARECIDA SOUSA SILVA LEAL",
+    "RAUL RODRIGUES DE SOUSA",
+    "JULIA CUNHA SANTOS",
+    "RENATA APARECIDA SOUSA PEREIRA MOREIRA",
+    "RONEY SILVA FIGUEREDO",
+    "DARISSON DAMASCENO RODRIGUES",
+    "RODRIGO MARTINS PEREIRA",
+    "DIEGO RIBEIRO LIMA",
+    "CORALINE APARECIDA DA SILVA",
+    "MARCELA LACERDA RIBEIRO PENA",
+    "ROBERTO CIRELLI",
+    "ANTONIO FRANCISCO DE SENA COSTA",
+    "MARCOS ALEXANDRE DE BARROS",
+    "LUCAS TORRES DE AZEVEDO",
+    "ROBSON VAGNER COSTA MARAN",
+    "APARICIO CARLOS DO NASCIMENTO JUNIOR",
+    "ELTON FERNANDES DOS SANTOS",
+    "YURI BERTAN NAGUARA",
+    "CIMAR CRUZ DA ROCHA",
+    "ANTONIA PEREIRA DE LIMA",
+    "LUCAS IGOR ALVES PEREIRA",
+    "EDISON SILVA DE OLIVEIRA",
+    "KALIL FELIPE DA SILVA",
+    "CLEOVAN DA SILVA ARAUJO",
+    "ANTONIO CARLOS GOMES RESENDE",
+    "JOAO FELIPE TEIXEIRA DOS SANTOS",
+    "KAIQUE TIBERIO FRANCO",
+    "FERNANDA BATISTA GUALANDI",
+    "MARCOS VINICIUS SILVA LIMA",
+    "ADAM PEREIRA RODRIGUES",
+    "RAFAEL LIMA DE ALMEIDA",
+    "ALESSANDRO DA PAZ MENDES",
+    "MARKUS VINICIUS MELO PORTO",
+    "SERGIO FELLIPE DO NASCIMENTO DE ARAUJO",
+    "GUILHERME VINICIUS DE ALMEIDA",
+    "MARCO AURELIO ABRANCHES",
+    "DANIELE ALVES ARAUJO",
+    "JONATHAN KAUA SANTOS DA SILVA",
+    "ANNA CAROLINA OLIVEIRA GOMES",
+    "VITORIA EVELLYN DA SILVA MARTINS",
+    "YASMIN HUBNER RIBEIRO",
+    "CAUA MONTEIRO SILVA",
+    "ADRIANO MARTINS MARIANO",
+    "GEOVANNA SOUZA PRADO ALVES",
+    "CINTIA CRISTINA BATISTA BARRETO",
+    "ANDERSON OLIVEIRA DA SILVA",
+    "EDIVAN DE JESUS COSTA",
+    "EDILSON FERREIRA DOS SANTOS",
+    "PIETRO DE JESUS LIMA",
+    "YASMIN DE FRANCA LIMA",
+    "MATHEUS HENRIQUE DA SILVA OLIVEIRA",
+    "RENAN KEIJI UEMA",
+    "TIAGO CARVALHO DE SOUSA",
+    "CARINA ALVES VICENTINI",
+    "MARIA LUIZI KRAUZE",
+    "EVILASIO ALVES DE OLIVEIRA",
+    "MARCELA BONFIM SOUZA DE OLIVEIRA",
+    "VITOR LIMA DE SENA COSTA",
+    "JOSE FERNANDO RODRIGUES DE SENA",
+    "JOSE MILTON FERREIRA DA SILVA",
+    "GABRIEL BERTOLDO GONCALVES",
+    "EDDY RHERISON DA SILVA LISBOA",
+    "ANTONIO CARLOS DE LUCAS",
+    "PAMELA MONIQUE PEGADO DE SOUZA",
+    "WELLINGTON PAES SANTANA",
+    "JOCEAN DELFINO DA SILVA",
+    "EDUARDO DA SILVA PEREIRA",
+    "VICTOR ENZO MUNIZ DA SILVA",
+    "ADEILDO TELES DA SILVA",
+    "RENE DA SILVA",
+    "CELIO SOUZA GAMA",
+    "RICHARD MATHEUS SANTOS FRANCO",
+    "WELLYGNTON DE SENA COSTA",
+    "CARLOS JOSE ALVES RODRIGUES",
+    "FRANCIS KLEY DE LIMA ROSA",
+    "KELVIN DOUGLAS SOUZA DE OLIVEIRA",
+    "ANTONIO MALAN COELHO RODRIGUES",
+    "JOAO PEDRO SILVA CORREA",
+    "VALDIR OLIVEIRA SOUZA",
+    "MARCOS VIEIRA CALDAS",
+    "JOSE RODRIGO DOS SANTOS MONTEIRO",
+    "ADAO FERNANDO DA SILVA",
+    "KARINA IZABEL SCHEFFER",
+    "PIETRO HENRIQUE DOS SANTOS FARIA",
+    "FRANCISCO ADRIANO BEZERRA",
+    "OSTIAN PALMEIRA DA SILVA JUNIOR",
+    "JANAINA ALBUQUERQUE DO NASCIMENTO",
+    "VANUSA RIBEIRO DA SILVA",
+    "VINICIUS SOUSA SILVA",
+    "JOSE ARNALDO CECHINEL",
+    "THIAGO JUNIOR BAREA",
+    "NELSON GONCALVES VERAS",
+    "MAYARA CRISTINA SANTOS",
+    "HALBERT MARQUES PESSOA",
+    "ARTHUR SILVA VICENTE",
+    "THIAGO CESAR DE ABREU RIBEIRO",
+    "FRANCOIS SOUSA OLIVEIRA",
+    "DAVID B DOS SANTOS JUNIOR",
+    "EDUARDO SCARTOZZONI",
+    "JOAO OLIV LIMA DE QUEIROZ ALVE",
+    "PABLO ALEXANDRE DOS PASSOS",
+    "JOAO VITOR PIRES LEITE",
+    "JOSE ALBERTO SANTOS DE JESUS",
+    "FELIPE BENTO INACIO",
+    "GABRIEL LIMA DE ALMEIDA",
+    "LUIZ AUGUSTO SANTOS BARBOSA ALVES",
+    "ANDERSON LESZCZUK GOMES",
+    "ADRIANA LETICIA DE JESUS OLIVEIRA",
+    "JOSEVAN FRANCISCO DOS SANTOS",
+    "MAURICIO DE ANDRADE GENOFRE",
+    "FRANCISCO HUENES PEREIRA MOREIRA",
+    "MICHELLE MARTINEZ FERREIRA",
+    "ADRIANO LUIZ VOTTA BEZERRA",
+    "ALESSANDRO DIAS",
+    "ALINE TRAVAGLIA VANO",
+    "ANDRE LEMOS",
+    "ANDRE CAMARGO",
+    "ANTONIO CARLOS FERNANDES DE BRITO",
+    "ANTONIO RIBEIRO JUNIOR",
+    "BERENICE WOJTOWICZ ZACATEI",
+    "BRYAN SOUZA LIMA DA SILVA",
+    "CARLOS BERTASOLI",
+    "CAROLINE PINTO DE SOUZA",
+    "CESAR YUKIO SHIRAIWA",
+    "CLEBER BIONDO",
+    "CLODOALDO",
+    "DANIEL QUICOLI",
+    "DANIELA MATOS DE OLIVEIRA",
+    "EDUARDO DOMINGUES",
+    "FABIO TRIMARCO",
+    "FELIPE GOES",
+    "GLADSON MORTOZA",
+    "GLAUCIA ROSSETTI",
+    "IURY AIELLO",
+    "JEFERSON BRITO",
+    "JOAO PAULO SANTOS MOREIRA",
+    "JOE LOUIS",
+    "JOSE BLANCO",
+    "JOSE PEDRO",
+    "KLEBER SANCHES",
+    "LEANDRO DA SILVA MATTOS",
+    "LUIS FELIPE VIZZONI",
+    "LUIS FERNANDO BASSO",
+    "LUIZ PAULO ALVES DE CASTRO",
+    "MARCO AURELIO ABRANCHES",
+    "MARCO AURELIO GOMES",
+    "MARCO AURELIO OLIVEIRA",
+    "NELIO YUKIO KONNO",
+    "RENATO CAPELETTO",
+    "RENIERI",
+    "ROGERIO CESAR TIRADO",
+    "RONDINEI GOMES SANTANA",
+    "RUI GUILHERME VITOR NUNES",
+    "SAMANTHA RIBEIRO FARIA",
+    "WILTON SILVA",
+    "GILSON GRANZIER",
+    "LUIZ KAZUO",
+    "GUILHERME SALES",
+    "EDMILSON MOREIRA DA SILVA",
+    "FRANCINALDO ALVES DO NASCIMENTO",
+    "GENILDO ERNANI DOS SANTOS",
+    "IGOR ANTONIO TENORIO",
+    "JAIRO RIBEIRO DE CAMPOS",
+    "JEOVANE RODRIGUES DE SOUZA",
+    "JOSIAIS JOAQUIM DE SANTANA",
+    "LUIS HENRIQUE ROSEIRA DA SILVA",
+    "PAULO HENRIQUE COSTA DOS SANTOS",
+    "RAFAEL DOS SANTOS SOUSA",
+    "RISONALDO ALEXANDRE DE ARAUJO",
+    "TABATA CAROLINA SILVEIRA",
+    "CLAUDIO GIMENEZ",
+    "FREDERICO",
+    "MICHAEL ZAMORA",
+    "MATHIAS ALEJANDRO",
+    "JUAN ARAVENA",
+    "JOSE MONTENEGRO",
+    "IVAN GODOY",
+    "GILTON MALFIN",
+    "SERGIO CALQUI"
+];
+
+let defaultVencedores = [
+    { nome: "MARCIO GABRIEL DOS SANTOS", hora: "15:00", data: "12/06/2026" },
+    { nome: "RODRIGO GUZZINATI", hora: "15:15", data: "12/06/2026" }
 ];
 
 let participantes = JSON.parse(localStorage.getItem('mendes_participantes')) || [...defaultParticipantes];
-let vencedoresHistory = JSON.parse(localStorage.getItem('mendes_historico')) || [];
+let vencedoresHistory = JSON.parse(localStorage.getItem('mendes_historico')) || [...defaultVencedores];
+let todosParticipantes = JSON.parse(localStorage.getItem('mendes_todos_participantes')) || [...defaultParticipantes, "MARCIO GABRIEL DOS SANTOS", "RODRIGO GUZZINATI"];
+
+// Remove ganhadores anteriores da urna ativa (se usando defaults pela primeira vez)
+if (!localStorage.getItem('mendes_participantes')) {
+    const nomesVencedores = vencedoresHistory.map(v => v.nome);
+    participantes = participantes.filter(p => !nomesVencedores.includes(p));
+}
+
 
 const screens = {
     home: document.getElementById('home-screen'),
@@ -41,6 +357,11 @@ const btnCloseConfig = document.getElementById('close-config');
 const btnSaveParticipants = document.getElementById('btn-save-participants');
 const participantsInput = document.getElementById('participants-input');
 const btnClearHistory = document.getElementById('btn-clear-history');
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabContents = document.querySelectorAll('.tab-content');
+const btnShowOverlay = document.getElementById('btn-show-winners-overlay');
+const btnCloseOverlay = document.getElementById('btn-close-overlay');
+const winnersOverlay = document.getElementById('winners-overlay');
 
 function init() {
     atualizarContador();
@@ -48,6 +369,16 @@ function init() {
     btnNovoSorteio.addEventListener('click', resetParaHome);
     
     btnFullscreen.addEventListener('click', toggleFullScreen);
+    
+    // Lógica das Abas
+    tabBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            tabBtns.forEach(b => b.classList.remove('active'));
+            tabContents.forEach(c => c.classList.remove('active'));
+            btn.classList.add('active');
+            document.getElementById(btn.getAttribute('data-target')).classList.add('active');
+        });
+    });
     
     btnConfig.addEventListener('click', abrirModalConfig);
     btnCloseConfig.addEventListener('click', () => configModal.classList.remove('active'));
@@ -59,11 +390,117 @@ function init() {
     });
     btnCloseHistory.addEventListener('click', () => historyModal.classList.remove('active'));
     btnClearHistory.addEventListener('click', limparHistorico);
+    
+    btnShowOverlay.addEventListener('click', mostrarOverlayGanhadores);
+    btnCloseOverlay.addEventListener('click', () => winnersOverlay.classList.add('hidden'));
 }
 
 function abrirModalConfig() {
+    renderizarVisualizacao();
+    renderizarAbaGanhadores();
     participantsInput.value = participantes.join('\n');
     configModal.classList.add('active');
+}
+
+function renderizarVisualizacao() {
+    const listEl = document.getElementById('participants-view-list');
+    listEl.innerHTML = '';
+    
+    const winnerNames = new Map();
+    vencedoresHistory.forEach(v => {
+        winnerNames.set(v.nome, v.data || v.hora);
+    });
+    
+    const todosNomes = [...new Set([...todosParticipantes])];
+    
+    if (todosNomes.length === 0 && participantes.length === 0) {
+        listEl.innerHTML = '<li style="color: #888; list-style: none;">Nenhum participante cadastrado.</li>';
+        return;
+    }
+    
+    const aindaNaUrna = [...participantes];
+    const jaSorteados = todosNomes.filter(n => winnerNames.has(n));
+    const naoSorteados = todosNomes.filter(n => !winnerNames.has(n) && !aindaNaUrna.includes(n));
+    const listaFinal = [...aindaNaUrna, ...jaSorteados, ...naoSorteados];
+    
+    listaFinal.forEach(nome => {
+        const li = document.createElement('li');
+        const nameSpan = document.createElement('span');
+        nameSpan.textContent = nome;
+        li.appendChild(nameSpan);
+        
+        if (winnerNames.has(nome)) {
+            li.classList.add('winner-in-list');
+            const badge = document.createElement('span');
+            badge.className = 'winner-badge';
+            badge.textContent = `🏆 ${winnerNames.get(nome)}`;
+            li.appendChild(badge);
+        }
+        
+        listEl.appendChild(li);
+    });
+}
+
+function renderizarAbaGanhadores() {
+    const container = document.getElementById('winners-by-date');
+    container.innerHTML = '';
+    
+    if (vencedoresHistory.length === 0) {
+        container.innerHTML = '<p style="color: #888; text-align: center; padding: 20px;">Nenhum ganhador registrado ainda.</p>';
+        return;
+    }
+    
+    const grouped = {};
+    vencedoresHistory.forEach(v => {
+        const key = v.data || 'Sem data';
+        if (!grouped[key]) grouped[key] = [];
+        grouped[key].push(v);
+    });
+    
+    const sortedDates = Object.keys(grouped).sort((a, b) => {
+        const partsA = a.split('/'); const partsB = b.split('/');
+        const dA = new Date(partsA[2], partsA[1]-1, partsA[0]);
+        const dB = new Date(partsB[2], partsB[1]-1, partsB[0]);
+        return dB - dA;
+    });
+    
+    sortedDates.forEach(date => {
+        const group = document.createElement('div');
+        group.className = 'winners-date-group';
+        group.innerHTML = `<h3>📅 ${date}</h3>`;
+        const ul = document.createElement('ul');
+        grouped[date].forEach(v => {
+            const li = document.createElement('li');
+            li.innerHTML = `<strong>${v.nome}</strong><span>${v.hora}</span>`;
+            ul.appendChild(li);
+        });
+        group.appendChild(ul);
+        container.appendChild(group);
+    });
+}
+
+function mostrarOverlayGanhadores() {
+    const hoje = new Date().toLocaleDateString('pt-BR');
+    document.getElementById('overlay-date').textContent = hoje;
+    
+    const lista = document.getElementById('overlay-winners-list');
+    lista.innerHTML = '';
+    
+    const ganhadoresHoje = vencedoresHistory.filter(v => v.data === hoje);
+    
+    if (ganhadoresHoje.length === 0) {
+        lista.innerHTML = '<p style="color: #888; font-size: 1.2rem;">Nenhum ganhador sorteado hoje.</p>';
+    } else {
+        ganhadoresHoje.forEach(v => {
+            const div = document.createElement('div');
+            div.className = 'overlay-winner-name';
+            div.textContent = v.nome;
+            lista.appendChild(div);
+        });
+    }
+    
+    configModal.classList.remove('active');
+    winnersOverlay.classList.remove('hidden');
 }
 
 function toggleFullScreen() {
@@ -79,18 +516,41 @@ function toggleFullScreen() {
 }
 
 function salvarParticipantes() {
-    const lines = participantsInput.value.split('\n');
-    participantes = lines.map(line => line.trim()).filter(line => line.length > 0);
+    // Quebra por linha (suporta Enter normal)
+    const lines = participantsInput.value.split(/\r?\n/);
     
+    let parsed = [];
+    lines.forEach(line => {
+        // Remove números no início e aspas (simples ou duplas)
+        let cleanLine = line.replace(/^\d+[\.\-\)]\s*/, '').replace(/["']/g, '').trim();
+        
+        // Se a pessoa colar separado por vírgulas na mesma linha por acidente
+        if (cleanLine.includes(',') && cleanLine.length > 20) {
+            let subLines = cleanLine.split(',');
+            subLines.forEach(sub => {
+                if(sub.trim().length > 0) parsed.push(sub.trim());
+            });
+        } else if (cleanLine.length > 0) {
+            parsed.push(cleanLine);
+        }
+    });
+
+    participantes = parsed;
+    
+    todosParticipantes = [...new Set([...parsed, ...vencedoresHistory.map(v => v.nome)])];
+    localStorage.setItem('mendes_todos_participantes', JSON.stringify(todosParticipantes));
     localStorage.setItem('mendes_participantes', JSON.stringify(participantes));
     
     atualizarContador();
-    configModal.classList.remove('active');
+    
+    // Volta para a aba de visualizar
+    document.querySelector('.tab-btn[data-target="content-view"]').click();
+    renderizarVisualizacao();
     
     btnSortear.disabled = false;
     btnSortear.innerHTML = '<span class="btn-icon">⚽</span> SORTEAR AGORA!';
     btnSortear.style.opacity = "1";
-    alert('Participantes atualizados com sucesso!');
+    alert(`Sucesso! A urna agora tem ${participantes.length} participantes.`);
 }
 
 function limparHistorico() {
@@ -189,7 +649,8 @@ function escolherVencedor() {
     
     vencedoresHistory.push({
         nome: vencedor,
-        hora: new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' })
+        hora: new Date().toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' }),
+        data: new Date().toLocaleDateString('pt-BR')
     });
     
     localStorage.setItem('mendes_historico', JSON.stringify(vencedoresHistory));
